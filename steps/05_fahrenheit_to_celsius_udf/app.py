8209badfc115ae46c74499466d75123781439d1a
segotjiie <65173910+segotjiie@users.noreply.github.com>
2023-12-05 12:18:35 +0000

    changed the function

--- steps/05_fahrenheit_to_celsius_udf/app.py
+++ steps/05_fahrenheit_to_celsius_udf/app.py
@@ -1,20 +1,23 @@
-sno#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 # Hands-On Lab: Data Engineering with Snowpark
 # Script:       05_fahrenheit_to_celsius_udf/app.py
 # Author:       Jeremiah Hansen, Caleb Baechtold
 # Last Updated: 1/9/2023
 #------------------------------------------------------------------------------
-
+ 
 # SNOWFLAKE ADVANTAGE: Snowpark Python programmability
 # SNOWFLAKE ADVANTAGE: Python UDFs (with third-party packages)
 # SNOWFLAKE ADVANTAGE: SnowCLI (PuPr)
-
+ 
 import sys
+from scipy.constants import convert_temperature
 
+ 
 def main(temp_f: float) -> float:
-    return (float(temp_f) - 32) * (5/9)
+    return convert_temperature(float(temp_f),'F', 'C')
 
-
+ 
+ 
 # For local debugging
 # Be aware you may need to type-convert arguments if you add input parameters
 if __name__ == '__main__':
